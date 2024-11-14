ce85e19468f5a25ef19bb0afbd74cf09dcc31d12
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-11-14 20:35:02 +0530

    14: model validations added

--- Models/StudentDto.cs
+++ Models/StudentDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,11 @@ namespace tutWebApi.Models
     public class StudentDto
     {
         public int Id { get; set; }
+        [Required]
         public string? StudentName { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
+        [Required]
         public string? Address { get; set; }
     }
 }
