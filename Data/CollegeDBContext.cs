69fb4f9c1cf74f381f5ef70258c8c6b8bd3d4551
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-11-16 19:55:09 +0530

    30: code first: step 5-  add dummy data in db

--- Data/CollegeDBContext.cs
+++ Data/CollegeDBContext.cs
@@ -15,5 +15,31 @@ namespace tutWebApi.Data
         }
         //step 1: create table dataset
         DbSet<Student> Students { get; set; }
+
+        /*after overriding the below method and adding the dummy data, write below commands
+            -  dotnet ef migrations add AddDummyData
+            -  dotnet ef database update
+        */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(new List<Student>()
+            {
+                new Student
+                {
+                    Id = 1,
+                    StudentName = "Venkat",
+                    Address = "India,Hyd",
+                    Email = "Venkat@gmail.com"
+                },
+                new Student
+                {
+                    Id = 2,
+                    StudentName = "Venkat1",
+                    Address = "India,Hyd1",
+                    Email = "Venkat1@gmail.com"
+                },
+            }
+            );
+        }
     }
 }
