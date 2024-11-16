b06ac3cc54839abc89510eb705ea6f4c249fb715
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-11-16 20:42:19 +0530

    32: code first: step 7-  moving entity type configs to separate file

---
+++ Data/Config/StudentConfig.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace tutWebApi.Data.Config
+{
+    public class StudentConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable("Sudents");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(n => n.StudentName).IsRequired();
+            builder.Property(n => n.StudentName).HasMaxLength(250);
+            builder.Property(n => n.Address).IsRequired(false).HasMaxLength(500);
+            builder.Property(n => n.Email).IsRequired().HasMaxLength(250);
+
+            builder.HasData(
+                new List<Student>()
+                {
+                    new Student
+                    {
+                        Id = 1,
+                        StudentName = "Venkat",
+                        Address = "India,Hyd",
+                        Email = "Venkat@gmail.com"
+                    },
+                    new Student
+                    {
+                        Id = 2,
+                        StudentName = "Venkat1",
+                        Address = "India,Hyd1",
+                        Email = "Venkat1@gmail.com"
+                    },
+                }
+            );
+        }
+    }
+}
